74a48969c5dee3803c77c0f650c2bdbe6083371b
RMYanenko <yanenko.r.m@gmail.com>
2021-12-24 15:07:50 +0200

    add git

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+import gulp from "gulp";
+
+import { path } from "./gulp/config/path.js";
+
+//import plugins
+import { plugins } from "./gulp/config/plugins.js"
+
+global.app = {
+  path: path,
+  gulp: gulp,
+  plugins: plugins
+};
+
+//Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+
+
+//Наблюдатель за изменениями в файлах
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+}
+
+const mainTasks = gulp.parallel(copy, html);
+
+//Последовательность  выполнение задач
+const dev = gulp.series(reset, mainTasks, watcher);
+
+//Сценарий по умолчанию
+gulp.task("default", dev);
