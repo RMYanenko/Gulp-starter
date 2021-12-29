0fd33b33e9d873ae280a9e58f7dfb2a2d25f846b
RMYanenko <yanenko.r.m@gmail.com>
2021-12-29 14:04:43 +0200

    ADD PLUGIN

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,9 @@
-import replace from 'gulp-replace';
+import replace from 'gulp-replace'; //Поиск и замена
+import plumber from 'gulp-plumber'; //Обработка ошибок
+import notify from 'gulp-notify'; // Подсказки
 
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber: plumber,
+    notify: notify
 }
