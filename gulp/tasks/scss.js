e23d2b02be7f7b89ef7a776c5ef5b719e7b0b8cf
RMYanenko <yanenko.r.m@gmail.com>
2022-01-03 14:07:31 +0200

    scss plugin

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,44 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from'gulp-rename';
+
+import claenCss from 'gulp-clean-css'; //сжатие CSS
+import webpcss from 'gulp-webpcss'; //Webp img in css
+import autoprefixer from 'gulp-autoprefixer';
+import groupCssMediaQueries from 'gulp-group-css-media-queries';
+
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+  return app.gulp
+    .src(app.path.src.scss, { sourcemap: true })
+
+    .pipe(app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "SCSS",
+          message: "Error: <%= error.message %>"
+        })
+      ))
+    .pipe(app.plugins.replace(/@img\//g, '../img/'))
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(groupCssMediaQueries())
+    .pipe(webpcss({
+        webpClass: ".webp",
+        noWebpClass: ".no-webp"
+    }))
+    .pipe(autoprefixer({
+        grid: true,
+        overrideBrowserslist: ["last 3 versions"],
+        cascade: true
+    }))
+    .pipe(app.gulp.dest(app.path.build.css)) //не сжатая версия стилей
+    .pipe(claenCss())
+    .pipe(rename({
+        extname: ".min.css"
+    }))
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browserSync.stream());
+};
--- gulpfile.js
+++ gulpfile.js
@@ -16,15 +16,18 @@ import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
 
 
 //Наблюдатель за изменениями в файлах
 function watcher() {
   gulp.watch(path.watch.files, copy);
   gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
 }
 
-const mainTasks = gulp.parallel(copy, html);
+//основные задачи
+const mainTasks = gulp.parallel(copy, html, scss);
 
 //Последовательность  выполнение задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(server, watcher));
